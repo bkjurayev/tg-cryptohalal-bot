36e2d30d280f5f0ff1727648ab9760cfd6a843da
bkjurayev <jurayev94@bk.ru>
2024-07-27 17:21:23 +0500

    start mongodb

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ require('dotenv').config()
 const { bot } = require('./bot/bot');
 
 
-// const { default: mongoose } = require('mongoose');
-// mongoose.connect(process.env.MONGO_URL).then(() => console.log('Connected to database')).catch(error => console.log('Error while connecting to database', error))
+const { default: mongoose } = require('mongoose');
+mongoose.connect(process.env.MONGO_URL).then(() => console.log('Connected to database')).catch(error => console.log('Error while connecting to database', error))
